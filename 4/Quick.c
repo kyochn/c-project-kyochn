4c360535926857b0a1af82dc91916b453058ca8e
kyochn <is0586vi@ed.ritsumei.ac.jp>
2020-07-27 00:56:00 +0900

    Add sort

---
+++ 4/Quick.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int compare_int(const void *a, const void *b){
+    return *(int*)a - *(int*)b;
+}
+
+int main(){
+	int count,num;
+	scanf("%d",&count);
+	int data[count];
+	for (int i = 0; i < count; i++){
+		scanf("%d",&num);
+		data[i]=num;
+	}
+
+    qsort(data, count, sizeof(int), compare_int);
+
+	printf("result:");
+    for (int i = 0; i < count; i++) {
+		printf(" ");
+        printf("%d", data[i]);
+    }
+    printf("\n");
+}
