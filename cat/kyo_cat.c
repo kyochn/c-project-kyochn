a8b3e81b2e220d9a472022ddca1ac7f2be9c3255
kyosuke <is0586vi@ed.ritsumei.ac.jp>
2020-08-28 18:42:13 +0900

    change cat

--- cat/kyo_cat.c
+++ cat/kyo_cat.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
-  
+int main(int argc, char *argv[]) {
+    FILE *file[argc-1];
+    char str[256];
 
-  return 0;
+    printf("%d\n", argc);
+
+    if (argc < 2) {
+        printf("failed to open\n");
+        return 1;
+    }
+
+    /*  file[0] = fopen(argv[1], "r");
+      file[1] = fopen(argv[2], "r"); */
+
+    for (int i = 0; i < argc - 1; i++) {
+        file[i] = fopen(argv[i + 1], "r");
+        if(file[i]==NULL){
+          printf("failed to open\n\n");
+          continue;
+        }
+        while ((fgets(str, 256, file[i])) != NULL) {
+            printf("%s", str);
+        }
+        printf("\n\n");
+        fclose(file[i]);
+    }
+    return 0;
 }
