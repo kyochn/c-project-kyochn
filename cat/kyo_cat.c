f9be4cd29f62927bcd10d43efc968da32661c566
kyosuke <is0586vi@ed.ritsumei.ac.jp>
2020-08-28 20:08:25 +0900

    change cat

--- cat/kyo_cat.c
+++ cat/kyo_cat.c
@@ -1,24 +1,38 @@
+// export PATH=$PATH:$(pwd)
+#include <signal.h>
 #include <stdio.h>
+#include <string.h>
+
+volatile sig_atomic_t flag = 0;
+void handler(int sig) { flag = 1; }
 
 int main(int argc, char *argv[]) {
-    FILE *file[2];
+    FILE *file[argc - 1];
     char str[256];
+    char c;
 
-    printf("%d\n", argc);
+//    printf("%d\n", argc);
+//    printf("%s\n",argv[1]);
+
+    signal(SIGQUIT, handler);
 
     if (argc < 2) {
-        printf("failed to open\n");
-        return 1;
+        while (!flag) {
+            std:
+            c = getchar();
+            printf("%c", c);
+        }
     }
 
-    /*  file[0] = fopen(argv[1], "r");
-      file[1] = fopen(argv[2], "r"); */
+    if (!strcmp(argv[1],"-")) {
+        goto std;
+    } 
 
     for (int i = 0; i < argc - 1; i++) {
         file[i] = fopen(argv[i + 1], "r");
-        if(file[i]==NULL){
-          printf("failed to open\n\n");
-          continue;
+        if (file[i] == NULL) {
+            printf("no exist\n\n");
+            continue;
         }
         while ((fgets(str, 256, file[i])) != NULL) {
             printf("%s", str);
